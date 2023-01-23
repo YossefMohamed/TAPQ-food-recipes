81a1163b981d1f86c41b2cfe1a9165fd3d66bb0a
YossefMohamed <youssef171035@feng.bu.edu.eg>
2023-01-23 14:38:58 +0200

    add navbar

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='nav-container flex justify-between p-4 font-bold border-b-2 items-center'>
+        <div className="logo text-4xl">
+            طبق
+        </div>
+        <div className="menu flex justify-between items-center w-[33%]">
+          <div className="item cursor-pointer">
+          Recipes
+          </div>
+          <div className="item cursor-pointer">
+          Lifestyle
+          </div>
+          <div className="item cursor-pointer">
+          Blog
+          </div>
+        </div>
+
+        <div className="btn-primary">
+          Get Started
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors :{
+      "main" : "#e97b3e",
+      "light" : "white",
+      "tmain" : "black",
+      "tsecondary" : "white"
+    }
+    ,
+    extend: {},
+  },
+  plugins: [],
+}
