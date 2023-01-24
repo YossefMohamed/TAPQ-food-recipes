1940796260c81611a27c4dad31e3979850faa48e
YossefMohamed <youssef171035@feng.bu.edu.eg>
2023-01-24 13:09:45 +0200

    add main screen

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,29 +1,19 @@
-import React from 'react'
-import './navbar.css';
+import React from "react";
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='nav-container flex justify-between p-4 font-bold border-b-4 items-center shadow-lg '>
-        <div className="logo text-4xl">
-            ٌطبق
-        </div>
-        <div className="menu flex justify-between items-center w-[33%]">
-          <div className="item cursor-pointer">
-          Recipes
-          </div>
-          <div className="item cursor-pointer">
-          Lifestyle
-          </div>
-          <div className="item cursor-pointer">
-          Blog
-          </div>
-        </div>
+    <div className="nav-container flex justify-between p-4 font-bold items-center border-b-4">
+      <div className="logo text-4xl">ٌطبق</div>
+      <div className="menu flex justify-between items-center w-[33%]">
+        <div className="item cursor-pointer">Recipes</div>
+        <div className="item cursor-pointer">Lifestyle</div>
+        <div className="item cursor-pointer">Blog</div>
+      </div>
 
-        <div className="btn-primary">
-          Get Started
-        </div>
+      <div className="btn-primary">Get Started</div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Navbar from "./components/navbar/Navbar"
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Navbar from "./components/navbar/Navbar";
+import "./index.css";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <div className="mx-[150px]">
- <Navbar />
+    <Navbar />
 
-    <div className="main-screen">
-      
+    <div className="main-screen flex justify-between">
+      <div className="left flex flex-col justify-center flex-wrap flex-1 gap-5">
+        <div className="typo text-8xl">
+          Good Food
+          <br />
+          Good Mood
+        </div>
+        <span className="btn-primary">Get Started</span>
+      </div>
+
+      <div className="right flex items-center justify-center flex-1">
+        <div className="image-container ">
+          <div className="grid grid-cols-2 grid-rows-2 gap-2 rounded-3xl overflow-hidden">
+            <div className="image1 w-[250px] h-[200px]"></div>
+            <div className="image2 w-[250px] h-[200px]"></div>
+            <div className="image3 w-[250px] h-[200px]"></div>
+            <div className="image4 w-[250px] h-[200px]"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="featured">
+      <div className="title"> Featured recipes :</div>
     </div>
   </div>
 );
-
-
